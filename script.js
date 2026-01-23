b04ee90b5d65eec6bd488c30bbf8a5a4af42d35c
entongganteng <153502026+entongganteng@users.noreply.github.com>
2026-01-23 10:45:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// const wrapper = document.querySelector(".wrapper");
+// const question = document.querySelector(".question");
+// const gif = document.querySelector(".gif");
+// const yesBtn = document.querySelector(".yes-btn");
+// const noBtn = document.querySelector(".no-btn");
+// const btnGroup = document.querySelector(".btn-group");
+
+// yesBtn.addEventListener("click", () => {
+//   question.innerHTML = "HOREEE, tidak bisa di cancel bleeeeee!";
+//   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+//   btnGroup.style.display = "none";
+// });
+
+// noBtn.addEventListener("mouseover", () => {
+//   const noBtnRect = noBtn.getBoundingClientRect();
+//   const maxX = window.innerWidth - noBtnRect.width;
+//   const maxY = window.innerHeight - noBtnRect.height;
+
+//   const randomX = Math.floor(Math.random() * maxX);
+//   const randomY = Math.floor(Math.random() * maxY);
+
+//   noBtn.style.left = randomX + "px";
+//   noBtn.style.top = randomY + "px";
+// });
+
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
@@ -11,17 +36,11 @@ yesBtn.addEventListener("click", () => {
   btnGroup.style.display = "none";
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+noBtn.addEventListener("click", () => {
+  question.innerHTML = "yahhh :(( coba pilih lagi deh hehehe";
+  gif.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTB3MnFkazZ2djVkZXh0and0ZGd1bjI4bW91azQxZmtmajc1MDdyeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/otC3E9VpgzSdEDUglZ/giphy.gif";
 });
+
 
 
 
